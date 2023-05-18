bc72f234ab39875d2ce2ccc72acce2d67c0607f0
Matias Maldonado <katamaldonado03@gmail.com>
2023-05-17 21:54:04 -0300

    arreglando y todo lo perdido y usando las api para trabajar pagina principal y la del producto

---
+++ index.js
@@ -0,0 +1,37 @@
+async function MostrarCantidad(){
+    let response = await fetch('https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/statistics');
+    let objeto = await response.json();
+    
+    let cantidadProductos = objeto.amount_of_products;
+    let text = document.createTextNode(cantidadProductos);
+    document.getElementById("cantidadProductos").appendChild(text);
+}
+MostrarCantidad();
+/*
+let productos = [
+    {srcImagen: 'notebook1.jpg', nombre: 'Notebook Dell Inspiron 3505'},
+    {srcImagen: 'notebook2.jpg', nombre: 'Notebook Dell Inspiron 3506'},
+    {srcImagen: 'notebook3.webp', nombre: 'Notebook Dell Inspiron 3507'},
+    {srcImagen: 'notebook1.jpg', nombre: 'Notebook Dell Inspiron 3505'},
+];
+
+for (let producto of productos) {
+    let texto = document.createTextNode(producto.nombre)
+
+    let strong = document.createElement('strong');
+    strong.appendChild(texto);
+
+    let imagen = document.createElement('img');
+    imagen.src = producto.srcImagen;
+
+    let link = document.createElement('a');
+    link.href = 'producto.html'
+    link.appendChild(imagen)
+    link.appendChild(strong);
+
+    let contenedor = document.createElement('div');
+    contenedor.className = 'preview-producto';
+    contenedor.appendChild(link);
+
+    document.getElementById('listado-productos').appendChild(contenedor);
+}*/
--- script.js
+++ script.js
@@ -22,3 +22,93 @@ function agregarCometario(evento){
 }
 
 document.getElementById("boton-enviar-comentario").addEventListener('click', agregarCometario);
+
+
+
+async function cargarDatosProducto() {
+    let response = await fetch('https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/products/1');
+    let producto = await response.json();
+
+   
+   
+    let titulo = document.createTextNode(producto.title);
+    document.getElementById('titulo-producto').appendChild(titulo);
+
+    let descripcion = document.createTextNode(producto.description);
+    document.getElementById('texto-descripcion').appendChild(descripcion);
+    
+
+    document.getElementById('link').href = producto.factory_url;
+
+    document.getElementById('imagen').src = producto.image_url;
+
+    for (let notebook of producto.notebooksTypes){
+    let subTituloTipopc = document.createElement('h5');
+    subTituloTipopc.appendChild(document.createTextNode('tipoPc'))
+
+    let listaDesordenada = document.createElement('ul')
+    let precio = document.createElement('li')
+    let ram = document.createElement('li')
+
+
+    let precioTextNode =  document.createTextNode('Precio: '+ notebook.price);
+    let ramTextNode = document.createTextNode('Ram: '+ notebook.ramAmount);
+
+    precio.appendChild(precioTextNode);
+    ram.appendChild(ramTextNode);
+
+
+    listaDesordenada.appendChild(precioTextNode);
+    listaDesordenada.appendChild(ramTextNode);
+
+    document.getElementById('descripcion').appendChild(subTituloTipopc);
+    document.getElementById('descripcion').appendChild(listaDesordenada);
+
+
+    
+}
+
+
+
+
+/*
+    let imagen = objeto.image_url;
+    console.log(imagen)
+    let textImagen = document.createTextNode(imagen);
+    let img = document.createElement('img');
+    img.src = imagen
+    document.getElementById("imagen-producto").appendChild(img)
+
+    let descripcion = objeto.description;
+    console.log(descripcion)
+    let text3 = document.createTextNode(descripcion);
+    document.getElementById("descripcion1").appendChild(text3);
+
+    let url = objeto.factory_url;
+    console.log(url)
+    let a = document.getElementById("link");
+    a.href = url
+
+    let tipoNootebook = objeto.notebooksTypes;
+    console.log(tipoNootebook)
+
+    for (let tipo of productos.notebooksTypes){
+        let h3 = document.createElement('h3');
+        let textH3 = document.createTextNode('Tipo Computadora')
+        let lista = document.createElement('ul');
+        let precio = document.createElement('li');
+        let ram = document.createElement('li');
+        let preciotxt = document.createTextNode('precio: ' + tipoNootebook.precio)
+        let ramtxt = document.createTextNode('Ram: ' + tipoNootebook.ram)
+
+        precio.appendChild(preciotx)
+        ram.appendChild(ramtxt)
+
+        lista.appendChild(precio)
+        lista.appendChild(ram)
+    }
+*/
+
+}
+cargarDatosProducto();
+
