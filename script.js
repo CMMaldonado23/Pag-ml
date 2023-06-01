709f0b4d4070e4023775fcc721d04fd7036f2648
Matias Maldonado <katamaldonado03@gmail.com>
2023-06-01 03:27:43 -0300

    agregamos el carrito de compra con la suma de la compra y la eleliminacion de cada articulo

--- script.js
+++ script.js
@@ -75,11 +75,12 @@ const app = Vue.createApp({
         return{
             
                 "id": 1,
-                cantidadComprados: 0,
                 "title": "Notebook HP 14-dq2024la",
                 "description": "Computadora HP orientado para gama media. Procesador Intel® Core™ i3 de 11.ª generación.  Windows 10 Home 64.  Unidad de estado sólido PCIe® NVMe™ M.2 de 256 GB . Pantalla de 14 pulgadas.",
                 "image_url": "https://ar-media.hptiendaenlinea.com/catalog/product/8/V/8VW01LA-1_T1615590539.png",
                 "factory_url": "https://www.hp.com/ar-es/shop/notebook-hp-14-dq2024la-3v8j6la.html",
+                notebookSeleccionada:0,
+                notebooksCompradas:[],
                 "notebooksTypes": [
                   {
                     "ramAmount": "8 GB",
@@ -99,13 +100,36 @@ const app = Vue.createApp({
               }
         },
         methods:{
-          clickBotonCompra(){
-            this.cantidadComprados +=1;
+          clickBotonCompra(indice){
+            this.notebooksCompradas.push(this.notebooksTypes[indice])
+
           },
           cambiarImagen(imagenUrl){
             this.image_url =imagenUrl;
           },
+          seleccionarTipoPc(indice){
+            this.notebookSeleccionada = indice;
+          },
+          eliminarCompra(indice){
+            this.notebooksCompradas = this.notebooksCompradas.filter((value, index) => index !== indice);
+          }
+        },
+        computed: {
+          total() {
+            let total = 0;
+            for(let notebookComprada of this.notebooksCompradas){
+              total += notebookComprada.price;
+
+              
+            }
+            return total;
+          },
+          mostrarCarroDeCompras(){
+            return this.notebooksCompradas.length !== 0;
+          }
+          
         }
+
       
     
 })
